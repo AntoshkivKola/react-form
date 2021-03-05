11671e6544b966a7d4bde4450bdf47a7797507e4
Kola Antoshkiv <ak.pkqtor@gmail.com>
2021-03-05 21:37:39 +0200

    Add test example

--- src/components/Pages/SignUpPage/index.jsx
+++ src/components/Pages/SignUpPage/index.jsx
@@ -1,12 +1,38 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { SIGN_UP_SCHEMA } from '../../utils/validationSchemas.js';
 import styles from './SignUpPage.module.scss';
 
-class SignUpPage extends Component {
-  render () {
-    return <div>sign-up</div>;
-  }
-}
+const initialValues = {
+  email: '',
+  password: '',
+};
+
+const SignUpPage = props => {
+  const onSubmit = (values, formikBag) => {
+    console.log(values);
+    console.log(formikBag);
+    formikBag.resetForm();
+  };
+
+  return (
+    <Formik initialValues={initialValues} validationSchema={SIGN_UP_SCHEMA} onSubmit={onSubmit}>
+      {formikProps => {
+        console.log(formikProps);
+        return (
+          <Form>
+            <Field name='email'  />
+            <ErrorMessage name='email' component='span'/>
+            <Field name='password' type='password' />
+            <ErrorMessage name='password' component='span'/>
+            <Field type='submit' value='submit' />
+          </Form>
+        );
+      }}
+    </Formik>
+  );
+};
 
 SignUpPage.propTypes = {};
 
