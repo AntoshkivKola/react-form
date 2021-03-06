a6e233edc3da3e7876756905729ce48cbb000f4a
Kola Antoshkiv <ak.pkqtor@gmail.com>
2021-03-06 15:28:33 +0200

    Add LoginForm

--- src/components/forms/LoginForm/index.jsx
+++ src/components/forms/LoginForm/index.jsx
@@ -1,9 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styles from './LoginForm.module.scss';
+import styles from '../forms.module.scss';
+
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { LOGIN_SCHEMA } from '../../utils/validationSchemas.js';
+import Input from '../Input';
+
+const initialValues = {
+  email: '',
+  password: '',
+};
 
 const LoginForm = props => {
-  return <div></div>;
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={LOGIN_SCHEMA}
+      onSubmit={props.onSubmit}
+    >
+      <Form className={styles.form}>
+        <Input name='email' type='email' placeholder='Email' fullSize={true} />
+
+        <Input
+          name='password'
+          type='password'
+          placeholder='Password'
+          fullSize={true}
+        />
+
+        <Field className={styles.submit} type='submit' value='Login' />
+      </Form>
+    </Formik>
+  );
 };
 
 LoginForm.propTypes = {};
