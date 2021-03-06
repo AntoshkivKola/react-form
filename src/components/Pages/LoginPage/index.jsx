6b9ec3e7ba5526bbb2128f41054d430477a008b8
Kola Antoshkiv <ak.pkqtor@gmail.com>
2021-03-06 15:28:14 +0200

    Refact LoginPage

--- src/components/Pages/LoginPage/index.jsx
+++ src/components/Pages/LoginPage/index.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styles from '../SignUpPage/SignUpPage.module.scss';
+
 import Header from '../../Header';
 import LoginForm from '../../forms/LoginForm';
 const LoginPage = props => {
@@ -11,15 +12,9 @@ const LoginPage = props => {
     <div className={styles.main}>
       <Header location={props.location} />
       <div className={styles.container}>
-        <h1 className={styles.title}>CREATE AN ACCOUNT</h1>
-        <p className={styles.info}>
-          We always keep your name and email address private.
-        </p>
-        <LoginForm onSubmit={onSubmit} />
-        <p className={styles.warning}>
-          By clicking this button, you agree to our{' '}
-          <a href='#'>Terms of Service.</a>
-        </p>
+        <h1 className={styles.title}>LOGIN TO YOUR ACCOUNT</h1>
+
+        <LoginForm  onSubmit={onSubmit} />
       </div>
     </div>
   );
