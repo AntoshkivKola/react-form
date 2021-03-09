3c44f156b85d145fb48e26b18a28a33671095be1
Kola Antoshkiv <ak.pkqtor@gmail.com>
2021-03-09 10:09:53 +0200

    Refact validation schemes

--- src/utils/validationSchemas.js
+++ src/utils/validationSchemas.js
@@ -1,19 +1,28 @@
 import * as Yup from 'yup';
 
 export const NAME_SCHEMA = Yup.string()
-  .matches(/^[A-Z][a-z]{1,64}/, 'Name must be a valid name')
+  .matches(/^[A-Z][a-z]{1,64}$/, 'Name must be a valid name and start`s of uppercase letter')
   .required();
+
 export const DISPLAY_NAME_SHEMA = Yup.string()
-  .matches(/^(?=.*?[a-z])(?=.*?[0-9]).{4,}$/, 'Display name should be more than 4 characters and contain min one number')
+  .matches(
+    /^(?=.*?[a-z])(?=.*?[0-9]).{4,}$/,
+    'Display name should be more than 4 characters and contain min one number'
+  )
   .required();
 
 export const EMAIL_SCHEMA = Yup.string()
-  .email('Test check for email')
+  .email('Please enter a valid email address')
   .required();
 
 export const PASSWORD_SCHEMA = Yup.string()
-  .matches(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/)
+  .matches(
+    /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/,
+    'A minimum 8 characters password contains a combination of uppercase and lowercase letter and number'
+  )
   .required();
+
+export const PASSWORD_LOGIN_SCHEMA = Yup.string().required();
 
 export const SIGN_UP_SCHEMA = Yup.object({
   firstName: NAME_SCHEMA,
@@ -29,5 +38,5 @@ export const SIGN_UP_SCHEMA = Yup.object({
 
 export const LOGIN_SCHEMA = Yup.object({
   email: EMAIL_SCHEMA,
-  password: PASSWORD_SCHEMA,
-})
+  password: PASSWORD_LOGIN_SCHEMA,
+});
